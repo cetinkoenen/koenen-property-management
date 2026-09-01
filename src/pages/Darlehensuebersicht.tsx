364a5f7ea755327476c2493b8e604de2f39781bd
Cetinkoenen <info.koenen@gmail.com>
2026-09-01 08:56:37 +0200

    Fit loan plan table within property cards

--- src/pages/Darlehensuebersicht.tsx
+++ src/pages/Darlehensuebersicht.tsx
@@ -449,7 +449,7 @@ function PropertyLoanCard(props: {
           {!planSummaryLoading && planSummaryError ? <div style={styles.errorBox}>{planSummaryError}</div> : null}
           {!planSummaryLoading && !planSummaryError ? (
             <div style={{ ...styles.tableWrap, marginBottom: 0 }}>
-              <table style={{ ...styles.table, minWidth: 860 }}>
+              <table style={{ ...styles.table, minWidth: 820 }}>
                 <thead>
                   <tr>
                     <th style={styles.th}>Jahr</th>
